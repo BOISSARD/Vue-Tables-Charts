24e3037b2c41af249b769e8712ddc893e8bfb54e
BOISSARD Clement <clement.boissard@soprasteria.com>
2020-03-02 15:54:05 +0100

    Ajout : vue formulaire, tableau et graphiques

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Formulaire/>
+    <Tableau/>
+    <Graphiques/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Formulaire from './components/Formulaire'
+import Tableau from './components/Tableau'
+import Graphiques from './components/Graphiques'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Formulaire,
+    Tableau,
+    Graphiques
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+
 </style>
